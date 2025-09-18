9656491eaa81f18ba503c827bdaefec3053b0a9b
xxntmctx <xxntmctx@gmail.com>
2025-09-18 17:47:35 +0800

    推荐默认启用  fragment 配置

--- app/Protocols/Shadowrocket.php
+++ app/Protocols/Shadowrocket.php
@@ -96,6 +96,7 @@ class Shadowrocket extends AbstractProtocol
         $config = [
             'tfo' => 1,
             'remark' => $server['name'],
+            'fragment' => '1,40-60,30-50,tlshello', // 添加默认的 fragment 配置
             'alterId' => 0
         ];
         if ($protocol_settings['tls']) {
@@ -142,6 +143,7 @@ class Shadowrocket extends AbstractProtocol
         $config = [
             'tfo' => 1,
             'remark' => $server['name'],
+            'fragment' => '1,40-60,30-50,tlshello', // 添加默认的 fragment 配置
             'alterId' => 0
         ];
 
